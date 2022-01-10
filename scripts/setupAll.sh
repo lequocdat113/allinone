8e5810a0c805213c3b6128bff9a699f573a8cf07
Ghost.Vn <lequocdat113@gmail.com>
2022-01-10 10:41:11 +0700

    add script easy install

---
+++ scripts/setupAll.sh
@@ -0,0 +1,11 @@
+cd /home/dat09/allinone/ldap
+docker-compose up -d
+
+cd /home/dat09/allinone/keycloak
+docker-compose up -d
+
+cd /home/dat09/allinone/moodle
+docker-compose up -d
+
+cd /home/dat09/allinone/mediawiki
+docker-compose up -d
---
+++ scripts/stopAll.sh
@@ -0,0 +1,11 @@
+cd /home/haihoaadmin/allinone/ldap
+docker-compose stop
+
+cd /home/haihoaadmin/allinone/keycloak
+docker-compose stop
+
+cd /home/haihoaadmin/allinone/moodle
+docker-compose stop
+
+cd /home/haihoaadmin/allinone/mediawiki
+docker-compose stop
